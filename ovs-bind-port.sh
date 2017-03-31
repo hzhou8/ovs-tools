c03f59d5b32855c7877e71d0ac31e37f2ac4f839
Han Zhou <zhouhan@gmail.com>
2017-03-31 15:02:48 -0700

    Add comment for usage of ovs-bind-port.sh

--- ovs-bind-port.sh
+++ ovs-bind-port.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 # usage: ./ovs-bind-port.sh <uuid> <mac> <ip/mask>
+#
+# After creating logical port using Neutron port-create or directly
+# create in SDN e.g. ovn-nbctl, use this command to simulate VM/pod
+# creation on host. To test, use:
+# $ ip netns exec <uuid> <command>
+# <command> is any command supposed to test in VM/pod, such as ping
+
 tapname=tap`echo $1 | awk -F - '{ print $1 }'`
 echo add internal port $tapname and bind to logical port $1
 ovs-vsctl --may-exist add-port br-int $tapname -- set interface $tapname type=internal -- set interface $tapname external-ids:iface-id=$1
